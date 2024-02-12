411a890f61ba90f179e2a31c4ff39879aabe2fd2
Skylar <skylarc55@gmail.com>
2024-02-12 17:27:03 -0500

    Add mobile styles for book details page

--- src/components/BooksList/BookRow/BookRow.tsx
+++ src/components/BooksList/BookRow/BookRow.tsx
@@ -4,17 +4,17 @@ import style from './bookrow.module.css';
 
 const BookRow = ({ book }: { book: Book }) => {
   return (
-    <div className={style.container}>
-      <Link to={`/book/${book.google_id}`}>
-        <div className={style.coverContainer}>
-          <img className={style.coverImage} src={book.images.thumbnail} alt={`${book.title} cover`} />
-        </div>
-        <div className={style.bookInfo}>
-          <span className={style.bookTitle}>{book.title}</span>
-          <span className={style.bookAuthors}>{book.authors}</span>
-        </div>
-      </Link>
-    </div>
+    <Link to={`/book/${book.google_id}`}>
+      <div className={style.container}>
+          <div className={style.coverContainer}>
+            <img className={style.coverImage} src={book.images.thumbnail} alt={`${book.title} cover`} />
+          </div>
+          <div className={style.bookInfo}>
+            <span className={style.bookTitle}>{book.title}</span>
+            <span className={style.bookAuthors}>{book.authors}</span>
+          </div>
+      </div>
+    </Link>
   )
 }
 
