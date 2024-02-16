a1d2e03c0dc53b5608bcdb839ebce01e00612c99
Skylar <skylarc55@gmail.com>
2024-02-16 18:53:19 -0500

    Add menu, search icons, adjust BookCard mobile styling

--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -1,17 +1,26 @@
 import { Link } from 'react-router-dom';
 import Search from '../Search/Search';
 import style from './nav.module.css';
+import { IoMenu, IoSearch } from 'react-icons/io5';
+import { useState } from 'react';
 
 const Nav = () => {
+  const [showSearch, setShowSearch] = useState<boolean>(false);
+  const [showMenu, setShowMenu] = useState<boolean>(false);
+
   return (
     <div className={style.container}>
       <div className={style.navLeft}>
+        <IoMenu className={style.menuButton} onClick={() => setShowMenu(!showMenu)} />
         <Link to={'/'} className={style.logo}>Bookbind</Link>
-        <div>
+        <div className={style.menu}>
           <Link to={''}>Library</Link>
         </div>
       </div>
-      <Search />
+      <IoSearch className={style.searchButton} onClick={() => setShowSearch(!showSearch)}/>
+      <div className={`${style.searchbar} ${showSearch ? style.show : ''}`}>
+        <Search />
+      </div>
     </div>
   )
 }
